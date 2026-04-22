f5201ce98fab0329dc08aceb9f9e26e53f7035c7
LPR7124 <liviapr@al.insper.edu.br>
2026-04-22 07:20:30 -0300

    quest.2_guarda_dado

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,17 @@ def rolar_dados(n):
         i += 1
     return lista_dados
 
+def guardar_dado (dados_rolados, dados_guardados, índice):
+    if índice < len(dados_rolados):
+        dado = dados_rolados[índice]
+        dados_guardados.append(dado)
+        dados_rolados = dados_rolados[:índice] + dados_rolados[índice+1:] # pega o elemento antes do índice e o elemento depois do próximo da  lista(retira assim o que tem o indice indicado na lista)
+    return dados_rolados, dados_guardados
+
+# dados_rolados = [1, 3, 2]
+# dados_no_estoque = [1, 2]
+# dado_para_guardar = 1
+
+# print(guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar))
+
+
